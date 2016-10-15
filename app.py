73bf65bdf81e942be866f8af2e57213fa84f27f4
Kyle <kylezhou2002@hotmail.com>
2016-10-15 02:36:10 -0400

    skeleton of login

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, request, session, render_template
+from utils.validate_login import valid_login, log_the_user_in
 
 
 app = Flask(__name__)
@@ -10,9 +11,19 @@ def index():
         return render_template('')
     return render_template('login.html')
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    if request.
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['user'],
+                       request.form['pass']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ utils/validate_login.py
@@ -0,0 +1,5 @@
+def valid_login(username, password):
+    return True
+
+def log_the_user_in(username):
+    pass
